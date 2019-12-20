ae2ccfa4976f362c7d1e6756b5c74fc3b14c604a
Pradeep Bhosle <pradeep.bhosle8@gmail.com>
2019-12-20 13:21:18 +0530

    Create loopingPagination.js

---
+++ loopingPagination.js
@@ -0,0 +1,440 @@
+const posts = [
+ {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 11,
+    "title": "vero rerum temporibus dolor",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 12,
+    "title": "ipsa repellendus fugit nisi",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 13,
+    "title": "et doloremque nulla",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 14,
+    "title": "repellendus sunt dolores architecto voluptatum",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 15,
+    "title": "ab voluptatum amet voluptas",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 16,
+    "title": "accusamus eos facilis sint et aut voluptatem",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 17,
+    "title": "quo laboriosam deleniti aut qui",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 18,
+    "title": "dolorum est consequatur ea mollitia in culpa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 19,
+    "title": "molestiae ipsa aut voluptatibus pariatur dolor nihil",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 20,
+    "title": "ullam nobis libero sapiente ad optio sint",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 21,
+    "title": "suscipit repellat esse quibusdam voluptatem incidunt",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 22,
+    "title": "distinctio vitae autem nihil ut molestias quo",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 23,
+    "title": "et itaque necessitatibus maxime molestiae qui quas velit",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 24,
+    "title": "adipisci non ad dicta qui amet quaerat doloribus ea",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 25,
+    "title": "voluptas quo tenetur perspiciatis explicabo natus",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 26,
+    "title": "aliquam aut quasi",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 27,
+    "title": "veritatis pariatur delectus",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 28,
+    "title": "nesciunt totam sit blanditiis sit",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 29,
+    "title": "laborum aut in quam",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 30,
+    "title": "nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 31,
+    "title": "repudiandae totam in est sint facere fuga",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 32,
+    "title": "earum doloribus ea doloremque quis",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 33,
+    "title": "sint sit aut vero",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 34,
+    "title": "porro aut necessitatibus eaque distinctio",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 35,
+    "title": "repellendus veritatis molestias dicta incidunt",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 36,
+    "title": "excepturi deleniti adipisci voluptatem et neque optio illum ad",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 37,
+    "title": "sunt cum tempora",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 38,
+    "title": "totam quia non",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 39,
+    "title": "doloremque quibusdam asperiores libero corrupti illum qui omnis",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 40,
+    "title": "totam atque quo nesciunt",
+    "completed": true
+  },
+  {
+    "userId": 3,
+    "id": 41,
+    "title": "aliquid amet impedit consequatur aspernatur placeat eaque fugiat suscipit",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 42,
+    "title": "rerum perferendis error quia ut eveniet",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 43,
+    "title": "tempore ut sint quis recusandae",
+    "completed": true
+  },
+  {
+    "userId": 3,
+    "id": 44,
+    "title": "cum debitis quis accusamus doloremque ipsa natus sapiente omnis",
+    "completed": true
+  },
+  {
+    "userId": 3,
+    "id": 45,
+    "title": "velit soluta adipisci molestias reiciendis harum",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 46,
+    "title": "vel voluptatem repellat nihil placeat corporis",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 47,
+    "title": "nam qui rerum fugiat accusamus",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 48,
+    "title": "sit reprehenderit omnis quia",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 49,
+    "title": "ut necessitatibus aut maiores debitis officia blanditiis velit et",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 50,
+    "title": "cupiditate necessitatibus ullam aut quis dolor voluptate",
+    "completed": true
+  },
+  {
+    "userId": 3,
+    "id": 51,
+    "title": "distinctio exercitationem ab doloribus",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 52,
+    "title": "nesciunt dolorum quis recusandae ad pariatur ratione",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 53,
+    "title": "qui labore est occaecati recusandae aliquid quam",
+    "completed": false
+  },
+  {
+    "userId": 3,
+    "id": 54,
+    "title": "quis et est ut voluptate quam dolor",
+    "completed": true
+  },
+  {
+    "userId": 3,
+    "id": 55,
+    "title": "voluptatum omnis minima qui occaecati provident nulla voluptatem ratione",
+    "completed": true
+  }
+];
+
+// using for loope data impliments
+
+let result = `<table class='table table-bordered ' id='data'>
+	<thead class='thead-light'>
+	<tr>
+	<th>ID</th>
+	<th>User Id</th>
+	<th>Title</th>
+	<th>completed</th>
+	</tr>
+	</thead>
+	`;
+
+for(let i = 0; i < posts.length; i++)
+	{
+		result += `
+			<tbody>
+			<tr>
+			<td>${posts[i].id}</td>
+			<td>${posts[i].userId}</td>
+			<td>${posts[i].title}</td>
+			<td>${posts[i].completed}</td>
+			</tr>
+			</tbody>`;
+	};
+result += `</table>`;
+
+document.getElementById('companyInfo').innerHTML = result;		
+
+// pagination table 
+
+// get the table element
+var $table = document.getElementById("data"),
+// number of rows per page
+$n = 10,
+// number of rows of the table
+$rowCount = $table.rows.length,
+
+
+
+// get the first cell's tag name (in the first row)
+$firstRow = $table.rows[0].firstElementChild.tagName,
+
+
+// boolean var to check if table has a head row
+$hasHead = ($firstRow === "TH"),
+
+// an array to hold each row
+$tr = [],
+// loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
+$i,$ii,$j = ($hasHead)?1:0,
+
+// holds the first row if it has a (<TH>) & nothing if (<TD>)
+$th = ($hasHead?$table.rows[(0)].outerHTML:"");
+console.log($j);
+// count the number of pages
+var $pageCount = Math.ceil($rowCount / $n);
+console.log($pageCount);
+// if we had one page only, then we have nothing to do ..
+if ($pageCount > 1) {
+    // assign each row outHTML (tag name & innerHTML) to the array
+    for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
+        $tr[$ii] = $table.rows[$i].outerHTML;
+
+    // create a div block to hold the buttons
+    $table.insertAdjacentHTML("afterend","<div id='buttons'></div");
+       // the first sort, default page is the first one
+    sort(1);
+}
+
+// ($p) is the selected page number. it will be generated when a user clicks a button
+function sort($p) {
+    /* create ($rows) a variable to hold the group of rows
+    ** to be displayed on the selected page,
+    ** ($s) the start point .. the first row in each page, Do The Math
+    */
+    var $rows = $th,$s = (($n * $p)-$n);
+    for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
+        $rows += $tr[$i];
+    
+    // now the table has a processed group of rows ..
+    $table.innerHTML = $rows;
+    // create the pagination buttons
+    document.getElementById("buttons").innerHTML = pageButtons($pageCount,$p);
+    // CSS Stuff
+    document.getElementById("id"+$p).setAttribute("class","active");
+}
+
+
+// ($pCount) : number of pages,($cur) : current page, the selected one ..
+function pageButtons($pCount,$cur) {
+    /* this variables will disable the "Prev" button on 1st page
+       and "next" button on the last one */
+    var $prevDis = ($cur == 1)?"disabled":"",
+        $nextDis = ($cur == $pCount)?"disabled":"",
+        /* this ($buttons) will hold every single button needed
+        ** it will creates each button and sets the onclick attribute
+        ** to the "sort" function with a special ($p) number..
+        */
+        $buttons = "<input type='button' value='&lt;&lt; Prev' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
+    for ($i=1; $i<=$pCount;$i++)
+        $buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
+    $buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
+    return $buttons;
+}
+
+
